64a6cf3bbaceab654c3cae85b3f0d86d0ad7f8a5
Tomasz Szandala <Tomasz.Szandala@gmail.com>
2021-08-08 15:05:10 +0200

    Fix

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -19,17 +19,17 @@ EOF
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename      = vars.lambda_filename
-  function_name = vars.lambda_name
+  filename      = var.lambda_filename
+  function_name = var.lambda_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = vars.lambda_handler
+  handler       = var.lambda_handler
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256(vars.lambda_filename)
+  source_code_hash = filebase64sha256(var.lambda_filename)
 
-  runtime = vars.lambda_runtime
+  runtime = var.lambda_runtime
 
 #   environment {
 #     variables = {
